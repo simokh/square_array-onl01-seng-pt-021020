c6211f592eab1cd534aebe759dbd90da7b922a0f
Simo Khouili <m.simokhouili@gmail.com>
2020-02-17 20:26:47 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 
 def square_array(array)
     new_array = []
-    count = 1 
+    count = 1
     array.each {|count| new_array << count ** 2 }
-    count += 1 
-    return new_array 
-  end 
+    count += 1
+    return new_array
+  end
